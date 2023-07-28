85881a56e40ec2bc80a68f25ecc4b74efd27067d
Amritansh Sharma <111220288+drowsy-coder@users.noreply.github.com>
2023-07-28 16:02:50 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Amritansh Sharma",
+  title: "Flutter Developer",
+  email: "amritansh.code@gmail.com",
+  gitHub: "drowsy-coder",
+  instagram: "drowsycoder",
+  linkedIn: "drowsy-coder",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "drowsycoder",
+  youTube: "drowsycoder",
 };
 
 const primaryColor = "#4E567E";
